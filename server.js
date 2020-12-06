07bfa06e2588b848fdd34462c08e9bca0b2f4289
G_2PRO_D2_Valentim Oliveira Jose Pedro <51673544+TRGZeca@users.noreply.github.com>
2020-12-06 18:05:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('./config/db');
 const Beer = require('./models/BeerModel');
 
 
-//app.set('port',process.env.PORT || 3000);
+app.set('port',process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 
