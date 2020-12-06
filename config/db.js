84f4118a4194627f8597a0acf75e87bf833a18b1
Jose Pedro Valentim Oliveira <s106786@ap.be>
2020-12-06 17:44:01 +0100

    fresh start

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const db = process.env.MongoURI;
-
-
-mongoose.connect(db,{useNewUrlParser : true,useUnifiedTopology:true})
-.then(()=>console.log('mongoDB Connected'))
-.catch((err)=>console.log(err));
