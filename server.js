75ce990975e1f593bf484f3a68325447e552bca7
Jose Pedro Valentim Oliveira <s106786@ap.be>
2020-12-06 18:18:40 +0100

    try2

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
+
 require('dotenv').config();
-const PORT = process.env.PORT;
+
 
 require('./config/db');
+
 const Beer = require('./models/BeerModel');
 
+//server
 
-//app.set('port',process.env.PORT || 3000);
+
+app.set('port',process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 
-
-app.use(express.json());
-
+// Bodyparser
 app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
@@ -32,5 +34,24 @@ app.post('/saveBeer',(req,res)=>{
     const {beer_name,beer_percentage,beer_type,beer_img} = req.body;
 })
 
-app.listen(PORT,()=>{console.log(`backend up on port: ${PORT}`)})
+
+
+//pagina 404
+app.use(function(req,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+//pagine 500 (= interne serverfout)
+app.use(function(req,res){
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Express started on http://localhost:${
+      app.get('port')}; press Ctrl-C to terminate.`);
+});
 
