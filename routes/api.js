df33e3127a77e5bc23f0ff21ae7c34ceeac85ae5
G_2PRO_D2_Valentim Oliveira Jose Pedro <51673544+TRGZeca@users.noreply.github.com>
2020-12-06 20:36:02 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Beer = require('../models/BeerModel');
 
 
-router.get('/getAllBeers',async(req,res)=>{
+router.get('/getAllBeers',(req,res)=>{
    let beersJson = await Beer.find().catch();
    res.json(beersJson);
     
